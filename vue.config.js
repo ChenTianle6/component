337d070d6ec8cc0911e233d0163f1c3095625d4f
ctl <3054008136@qq.com>
2022-07-18 18:56:48 +0800

    feat(新功能)封装button组件

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+/**
+ * @author YangLing
+ * @date 2022/7/18 11:17
+ */
+module.exports = {
+  devServer: {
+    port: 9999
+  },
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: "@import './src/styles/scssconfig.scss';"
+      }
+    }
+  }
+}
